5e4a999af22044c1a583ea014b7bd05c762edcb1
Haydart <makowiecki.radoslaw@gmail.com>
2018-07-18 10:25:26 +0200

    Refactor code

--- bot/main.py
+++ bot/main.py
@@ -1,22 +1,30 @@
 from pallegro.pallegro import AllegroWebApi
 
-
+API_KEY = 'a6f31c8c587740d796de5c6c6e01dadc'
+COUNTRY = 'PL'
+SANDBOX = False
+USERNAME = 'radmack'
+PASSWORD = 'SyncMaster14'
+TOKEN = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzE5NDQ5NjgsInVzZXJfbmFtZSI6IjI4MTczNDc2IiwianRpIjoiMzczMDllYTMtZTM3ZS00M2NjLTk2ODMtMjg1NDViYmZjOTRiIiwiY2xpZW50X2lkIjoiYTZmMzFjOGM1ODc3NDBkNzk2ZGU1YzZjNmUwMWRhZGMiLCJzY29wZSI6WyJhbGxlZ3JvX2FwaSJdfQ.VWqWz2Yl-D83dii-VDOwxbUfX9gP7BJe1v4UA70lQ4MtY5bXVRI_4L--aAtyReQoESOq1N0A1IvTTJ1lwA13BA5yxuJfmjvNuIdwpi46fIP9FGXR2SLIQHoNIKncOMRVVVtVShJMz8a5r04Dj2xBM7ngYbEEjT5krbbZh_LnjkvfJGOQ4zqE-9Qafwf5Jg3WzYWhuFzkISzlk5-_0x8DcY8TB6VRisN6Ryb3uborcvkNwjmRzBEqq9-yWRmvjBjt6WRYaU3hqoMi6xOwrjZVTt0mj01IEeS9GDZBpuxVF5O0ZyNLrAYpYA0bUDNPfpSYpHL9zUShSlqOCgQPkFD_Cg'
 
 
 def launch_bot():
-    login_to_api()
+    allegro = init_allegro()
+    user = login(allegro)
 
-
-def login_to_api():
-    allegro_api = AllegroWebApi(API_KEY, SANDBOX, COUNTRY)
-    user = allegro_api.login(USERNAME, PASSWORD)
     print(user)
-
     # buy_request = user.buy_or_bid(offer_id=7445656286, price=5, amount=1, buy_now=True)
-
-    huawei_id = allegro_api.get_user_id("SmA-Huawei1")
+    huawei_id = allegro.get_user_id("SmA-Huawei1")
     print(huawei_id)
-    print(allegro_api.get_user_public_data(user_id=huawei_id))
+    print(allegro.get_user_public_data(user_id=huawei_id))
+
+
+def login(allegro):
+    return allegro.login(USERNAME, PASSWORD)
+
+
+def init_allegro():
+    return AllegroWebApi(API_KEY, SANDBOX, COUNTRY)
 
 
 if __name__ == '__main__':
