62c662c2d6b3be1b7d6e65799e32ef105b0f4790
Haydart <makowiecki.radoslaw@gmail.com>
2018-07-18 10:23:34 +0200

    Get public data

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ from pallegro.pallegro import AllegroWebApi
 
 
 
+
 def launch_bot():
     login_to_api()
 
@@ -9,11 +10,13 @@ def launch_bot():
 def login_to_api():
     allegro_api = AllegroWebApi(API_KEY, SANDBOX, COUNTRY)
     user = allegro_api.login(USERNAME, PASSWORD)
-    # buy_request = user.buy_or_bid(offer_id=7445656286, price=5, amount=1, buy_now=True)
-    print(allegro_api.get_user_id("SmA-Huawei1"))
-    print(allegro_api.get_user_id("SmA-Huawei1"))
-
     print(user)
+
+    # buy_request = user.buy_or_bid(offer_id=7445656286, price=5, amount=1, buy_now=True)
+
+    huawei_id = allegro_api.get_user_id("SmA-Huawei1")
+    print(huawei_id)
+    print(allegro_api.get_user_public_data(user_id=huawei_id))
 
 
 if __name__ == '__main__':
