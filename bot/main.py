7277be89a5b3791c877a7b24c3e50d600928563f
Haydart <makowiecki.radoslaw@gmail.com>
2018-07-18 11:00:23 +0200

    Request all products in infinite loop

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import requests
 
 from pallegro.pallegro import AllegroWebApi
@@ -8,12 +10,17 @@ def launch_bot():
     user = login(allegro)
 
     print(user)
-    # buy_request = user.buy_or_bid(offer_id=7445656286, price=5, amount=1, buy_now=True)
     huawei_id = allegro.get_user_id("SmA-Huawei1")
     print(huawei_id)
     print(allegro.get_user_public_data(user_id=huawei_id))
 
-    fetch_huawei_auctions()
+    while True:
+        items = fetch_huawei_items()
+        all_items = [(item['id'], item['sellingMode']['price']['amount'], item['name'])
+                     for item in items['items']['promoted'] + items['items']['regular']]
+        for item in all_items:
+            print(item)
+        sleep(.5)
 
 
 def login(allegro):
@@ -24,13 +31,12 @@ def init_allegro():
     return AllegroWebApi(API_KEY, SANDBOX, COUNTRY)
 
 
-def fetch_huawei_auctions():
+def fetch_huawei_items():
     endpoint = 'https://api.allegro.pl/offers/listing?seller.id=47827759'
     headers = {"Authorization": "Bearer %s" % TOKEN, "Accept": "application/vnd.allegro.public.v1+json"}
     response = requests.get(endpoint, headers=headers)
-    huawei_auctions = response.json()
-    print(huawei_auctions)
-    return huawei_auctions
+    huawei_items = response.json()
+    return huawei_items
 
 
 if __name__ == '__main__':
